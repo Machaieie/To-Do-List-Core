15ca398552cce7cde0b4e4012419c5af87e52317
Machaieie <machaieieedwin1@gmail.com>
2024-02-16 12:37:00 +0200

    uploading the project

---
+++ src/main/java/com/todolistcore/ToDoListCore/ToDoListCoreApplication.java
@@ -0,0 +1,13 @@
+package com.todolistcore.ToDoListCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolistcore/ToDoListCore/ToDoListCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolistcore.ToDoListCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
